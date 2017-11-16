ef9618961030bdc05954a283d170d5fa80175dee
Your Name <you@example.com>
2017-11-16 12:24:36 +0100

    fixed compile script

--- university/wste/3/UT/hello_world_easy_unit_test/compile.sh
+++ university/wste/3/UT/hello_world_easy_unit_test/compile.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-cd ~/uczelnia/wste/3/ut/hello_world_easy_unit_test || exit 1
+# create buld
+mkdir -p build || exit 1
+rm -rf build/* || exit 1
 
-rm -rf build || exit 1
-mkdir build || exit 1
 
+# compile
 cd build/ || exit 1
- 
  cmake .. || exit 1
-
  make -j || exit 1
 
+
+echo -e "\n----------------------------\n1. run a program\n\n"
+./hello_world_program/hello_world_program
+
+echo -e "\n----------------------------\n2. run a test\n\n"
+./hello_world_program_tests/hello_world_program_tests
 
